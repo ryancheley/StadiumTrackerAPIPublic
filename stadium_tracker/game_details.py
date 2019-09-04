aaafc8af5bf8a440e201d3d064d97d4c3893f9ee
Ryan Cheley <rcheley@gmail.com>
2019-09-03 18:56:56 -0700

    refactored get method on GamesSeenCreate view to move the heavy lifting to a method in game_details.py

--- stadium_tracker/game_details.py
+++ stadium_tracker/game_details.py
@@ -61,3 +61,42 @@ def get_teams():
     for i in range(len(teams)):
         team_display.append({'id': teams[i].get('id'), 'name': teams[i].get('name')})
     return team_display
+
+
+def get_form_details(request):
+    sportId = 1
+    team1 = request.GET.get('team1')
+    team2 = request.GET.get('team2')
+    teamId = f'{team1},{team2}'
+    start_date = request.GET.get('start_date')
+    end_date = request.GET.get('end_date')
+    params = {
+        'sportId': sportId,
+        'teamId': teamId,
+        'startDate': start_date,
+        'endDate': end_date
+    }
+    url = 'http://statsapi.mlb.com/api/v1/schedule/games'
+    r = requests.get(url, params)
+    games_dates = r.json().get('dates')
+    display_dates = []
+    if games_dates is not None:
+        for i in range(len(games_dates)):
+            date = games_dates[i].get('date')
+            for j in range(len(games_dates[i].get('games'))):
+                away = games_dates[i].get('games')[j].get('teams').get('away').get('team').get('name')
+                away_id = games_dates[i].get('games')[j].get('teams').get('away').get('team').get('id')
+                home = games_dates[i].get('games')[j].get('teams').get('home').get('team').get('name')
+                home_id = games_dates[i].get('games')[j].get('teams').get('home').get('team').get('id')
+                away_score = games_dates[i].get('games')[j].get('teams').get('away').get('score')
+                home_score = games_dates[i].get('games')[j].get('teams').get('home').get('score')
+                text = f'{date}: {away} vs {home}. Final Score: {away_score} - {home_score}'
+                gamePk = games_dates[i].get('games')[j].get('gamePk')
+                data = {
+                    'text': text,
+                    'gamePk': gamePk
+                }
+                if (str(home_id) == team1 and str(away_id) == team2) or (
+                        str(home_id) == team2 and str(away_id) == team1):
+                    display_dates.append(data)
+    return  display_dates
--- stadium_tracker/views.py
+++ stadium_tracker/views.py
@@ -3,7 +3,7 @@ from django.views.generic.edit import DeleteView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.shortcuts import render
-from stadium_tracker.game_details import get_game_details, get_teams
+from stadium_tracker.game_details import get_game_details, get_teams, get_form_details
 
 import requests
 
@@ -42,45 +42,10 @@ class GamesSeenCreate(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy('stadium_tracker:gamesseen_list')
 
     def get(self, request, *args, **kwargs):
-        # TODO: Clean up this method as it seems really long
         # TODO: Fix issue with the last game returned being the game ID for all games displayed, look at FormSets
         form = GameSeenForm
-        sportId = 1
-        team1 = request.GET.get('team1')
-        team2 = request.GET.get('team2')
-        teamId = f'{team1},{team2}'
-        start_date = request.GET.get('start_date')
-        end_date = request.GET.get('end_date')
-        params = {
-            'sportId': sportId,
-            'teamId': teamId,
-            'startDate': start_date,
-            'endDate': end_date
-        }
-        url = 'http://statsapi.mlb.com/api/v1/schedule/games'
-        r = requests.get(url, params)
-        games_dates = r.json().get('dates')
-        display_dates = []
         teams = get_teams()
-        if games_dates is not None:
-            for i in range(len(games_dates)):
-                date = games_dates[i].get('date')
-                for j in range(len(games_dates[i].get('games'))):
-                    away = games_dates[i].get('games')[j].get('teams').get('away').get('team').get('name')
-                    away_id = games_dates[i].get('games')[j].get('teams').get('away').get('team').get('id')
-                    home = games_dates[i].get('games')[j].get('teams').get('home').get('team').get('name')
-                    home_id = games_dates[i].get('games')[j].get('teams').get('home').get('team').get('id')
-                    away_score = games_dates[i].get('games')[j].get('teams').get('away').get('score')
-                    home_score = games_dates[i].get('games')[j].get('teams').get('home').get('score')
-                    text = f'{date}: {away} vs {home}. Final Score: {away_score} - {home_score}'
-                    gamePk = games_dates[i].get('games')[j].get('gamePk')
-                    data = {
-                        'text': text,
-                        'gamePk': gamePk
-                    }
-                    if (str(home_id) == team1 and str(away_id) == team2) or (str(home_id) == team2 and str(away_id) == team1):
-                        display_dates.append(data)
-                        form = GameSeenForm(initial={'game_id': gamePk})
+        display_dates = get_form_details(request)
 
         context = {
             'form': form,
