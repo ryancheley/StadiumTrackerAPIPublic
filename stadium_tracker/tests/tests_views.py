95022c28530c2902a72706be0ccb8cee4b9de14e
Ryan Cheley <rcheley@gmail.com>
2019-09-08 07:50:36 -0700

    Added tests for GamesSeenDetailView

--- stadium_tracker/tests/tests_views.py
+++ stadium_tracker/tests/tests_views.py
@@ -50,3 +50,41 @@ class GamesSeenListViewTestCase(TestCase):
         response = self.client.get(reverse('stadium_tracker:gamesseen_list'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'stadium_tracker/gamesseen_list.html')
+
+
+@override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+class GamesSeenDetailViewTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        CustomUser.objects.create(
+            password='abc123',
+            is_superuser=False,
+            username='test',
+            first_name='test',
+            last_name='test',
+            email='test@test.com',
+            is_active=True,
+        )
+
+        games_to_test = [
+            448469,
+            491328,
+            565749,
+            566611,
+            8060,
+            566610,
+        ]
+
+        for game_id in games_to_test:
+            GamesSeen.objects.create(
+                game_id=game_id,
+                create_date=datetime.now,
+                modify_date=datetime.now,
+                delete_ind=False,
+                user_id=1,
+            )
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/stadium/games/1')
+        self.assertEqual(response.status_code, 200)
+
