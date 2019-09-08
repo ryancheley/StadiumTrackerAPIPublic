e2c11d48d9e8be88ae0623a599b5867288495f48
Ryan Cheley <rcheley@gmail.com>
2019-09-08 08:09:42 -0700

    Updated files for real implementation of django-nose

--- StadiumTrackerAPI/settings.py
+++ StadiumTrackerAPI/settings.py
@@ -104,7 +104,6 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # Tell nose to measure coverage on the 'foo' and 'bar' apps
 NOSE_ARGS = [
     '--with-coverage',
-    '--cover-package=foo,bar',
 ]
 
 # Database
