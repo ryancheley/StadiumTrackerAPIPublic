9dd5dec1be1d5b4fcc07e126ecdb39823fe34047
Ryan Cheley <rcheley@gmail.com>
2019-09-08 07:56:50 -0700

    Added tests for VenueList view

--- stadium_tracker/tests/tests_views.py
+++ stadium_tracker/tests/tests_views.py
@@ -95,5 +95,21 @@ class GamesSeenCreateTestCase(TestCase):
 
 
 @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
-class GamesSeenCreateTestCase(TestCase):
+class GamesSeenDeleteTestCase(TestCase):
     pass
+
+
+@override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+class VenueListTestCase(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/stadium/venues')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('stadium_tracker:venue_list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('stadium_tracker:venue_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'stadium_tracker/venue_list.html')
