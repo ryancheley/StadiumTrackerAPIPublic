2d9fa4b5af918d697ae0c56cc23f822d9e312dcb
Ryan Cheley <rcheley@gmail.com>
2019-09-22 10:43:36 -0700

    updated tests for models

--- stadium_tracker/tests/tests_models.py
+++ stadium_tracker/tests/tests_models.py
@@ -1,17 +1,12 @@
 from django.test import TestCase
-from stadium_tracker.models import GamesSeen
+from stadium_tracker.models import GamesSeen, GameDetails
 from users.models import CustomUser
+from datetime import datetime
 
 
-class GamesSeenModelTest(TestCase):
+class GameDetailsTest(TestCase):
 
-    def test_string_representation(self):
-        game = GamesSeen(game_id=8060, user_id=1)
-        self.assertEqual(game.game_id, 8060)
-        self.assertEqual(str(game), '8060')
-        self.assertNotEquals(str(game), 8060)
-
-    def create_gameseen(self):
+    def create_gamedetails(self, home_team, away_team, game_headline):
         CustomUser.objects.create(
             password='abc123',
             is_superuser=False,
@@ -22,15 +17,39 @@ class GamesSeenModelTest(TestCase):
             is_active=True,
         )
 
-        x = GamesSeen.objects.create(
+        x = GameDetails.objects.create(
             game_id=8060,
+            home_team=home_team,
+            home_runs=5,
+            home_hits=5,
+            home_errors=0,
+            away_team=away_team,
+            away_runs=0,
+            away_hits=0,
+            away_errors=0,
+            game_datetime=datetime(2019, 9, 27),
+            game_body='Lots of Text',
+            game_headline=game_headline,
+            venue_id=5,
             user_id=1,
-            venue_id=2395,
         )
         return x
 
-    def test_gameseen_creation(self):
-        g = self.create_gameseen()
-        self.assertTrue(isinstance(g, GamesSeen))
+    def test_gamedetail_creation(self):
+        g = self.create_gamedetails('Dodgers', 'Giants', 'Giants Suck!')
+        self.assertTrue(isinstance(g, GameDetails))
         self.assertEqual(g.game_id, 8060)
+        self.assertEqual(g.user_id, 1)
+        self.assertEqual(g.home_team, 'Dodgers')
+        self.assertEqual(g.home_runs, 5)
+        self.assertEqual(g.home_hits, 5)
+        self.assertEqual(g.home_errors, 0)
+        self.assertEqual(g.away_team, 'Giants')
+        self.assertEqual(g.away_runs, 0)
+        self.assertEqual(g.away_hits, 0)
+        self.assertEqual(g.away_errors, 0)
+        self.assertEqual(g.game_datetime, datetime(2019, 9, 27))
+        self.assertEqual(g.game_body, 'Lots of Text')
+        self.assertEqual(g.game_headline, 'Giants Suck!')
+        self.assertEqual(g.venue_id, 5)
         self.assertEqual(g.user_id, 1)
