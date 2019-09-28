a7c4d14153713bbeba1c3f921d2ae4aa526b13db
Ryan Cheley <rcheley@gmail.com>
2019-09-27 20:30:10 -0700

    Updated Navigation to have buttons and fixed URL for About to be based on Django URL instead of hard coded

--- content/urls.py
+++ content/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from content.views import ContentTemplateView
 
-app_name = 'stadium_tracker'
+app_name = 'content'
 
 
 urlpatterns = [
     path('', ContentTemplateView.as_view(template_name='page.html'), name='home'),
-    path('Pages/<name>', ContentTemplateView.as_view(template_name='page.html'), name='<name>'),
+    path('Pages/<str:name>', ContentTemplateView.as_view(template_name='page.html'), name='content'),
 ]
