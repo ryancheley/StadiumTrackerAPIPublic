f6ab9506d5742b44599f335066ebf009ad9f92b1
Ryan Cheley <rcheley@gmail.com>
2019-09-27 20:31:12 -0700

    Added users url data

--- StadiumTrackerAPI/urls.py
+++ StadiumTrackerAPI/urls.py
@@ -7,6 +7,7 @@ from django_registration.backends.activation.views import RegistrationView
 urlpatterns = [
     path('stadium/', include('stadium_tracker.urls')),
     path('backend/', admin.site.urls),
+    path('users/', include('users.urls')),
     url(r'^accounts/register/$', RegistrationView.as_view(form_class=CustomUserCreationForm), name='django_registration_register'),
     path(r'accounts/', include('django_registration.backends.activation.urls')),
     path(r'accounts/', include('django.contrib.auth.urls')),
