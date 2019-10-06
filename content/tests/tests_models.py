9ce55ea461581722672855b220890d71579510a8
Ryan Cheley <rcheley@gmail.com>
2019-10-06 09:43:30 -0700

    updated structure for content tests

---
+++ content/tests/tests_models.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from content.models import Content
+from users.models import CustomUser
+
+
+class ContentTestCase(TestCase):
+    def setUp(self) -> None:
+        CustomUser.objects.create(
+            username='ryan',
+            favorite_team=119,
+        )
+
+        Content.objects.create(
+            title='Home Page',
+            page_content='<h2>Home Page</h2><p>This is the home page!</p>',
+            author=CustomUser.objects.get(username='ryan'),
+        )
+
+    def test_home_page_created(self):
+        home = Content.objects.get(title='Home Page')
+        self.assertEqual(home.page_content, '<h2>Home Page</h2><p>This is the home page!</p>',)
+        self.assertEqual(home.title, 'Home Page')
+        self.assertEqual(home.author, CustomUser.objects.get(username='ryan'))
+
+    def test_string_representation(self):
+        page = Content.objects.get(title='Home Page')
+        self.assertEqual(str(page), 'Home Page')
